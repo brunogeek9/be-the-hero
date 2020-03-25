e87cb901a300e9d79e87ef89460517b660ab7cec
brunogeek9 <brunogeek9@gmail.com>
2020-03-25 19:47:52 -0300

    adição do componente de cadastro de incidente ao frontend

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -3,7 +3,7 @@ import {Route, Switch, BrowserRouter} from 'react-router-dom';
 import Logon from './pages/Logon';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
-
+import NewIncident from './pages/NewIncident';
 
 export default function Routes(){
     return(
@@ -12,6 +12,7 @@ export default function Routes(){
                 <Route path="/" exact component={Logon}></Route>
                 <Route path="/cadastro" component={Register}></Route>
                 <Route path="/perfil" component={Profile}></Route>
+                <Route path="/incidents/new" component={NewIncident}></Route>
 
 
             </Switch>
